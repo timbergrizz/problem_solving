13aa70bf5ccf305dd4f942ffe5e563d601315b6a
timbergrizzzz <50934118+timbergrizz@users.noreply.github.com>
2022-06-17 20:36:00 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y; cin >> x >> y;
+    if(x > 0) {
+        if(y > 0) cout << 1 << endl;
+        else cout << 4 << endl;
+    }
+    else{
+        if(y > 2) cout << 2 << endl;
+        else cout << 3 << endl;
+    }
+}
