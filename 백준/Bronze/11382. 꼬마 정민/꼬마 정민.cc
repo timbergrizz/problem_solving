b9a00b7912830787fb0bb7782c8ef998ab2a13f2
timbergrizzzz <50934118+timbergrizz@users.noreply.github.com>
2022-06-24 13:27:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -1,8 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-typedef long long ll;
 
 int main(){
-    ll a, b, c; cin >> a >> b >> c;
-    cout << a + b + c;
+   long long a, b, c; cin >> a >> b >> c;
+   cout << a + b + c;
 }
