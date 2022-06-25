b31cfec180331bde6faa9c2cd006fd1a47ee2b1f
timbergrizzzz <50934118+timbergrizz@users.noreply.github.com>
2022-06-25 15:45:42 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T; cin >> T;
+    string s1;
+    while(T--){
+        cin >> s1;
+        cout << s1.front() << s1.back() << "\n";
+    }
+}
