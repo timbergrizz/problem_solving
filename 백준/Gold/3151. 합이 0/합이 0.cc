d3dbed7171a634330547c1a54b758efdb562536a
timbergrizzzz <50934118+timbergrizz@users.noreply.github.com>
2022-06-30 12:31:21 +0900

    [Gold V] Title: 합이 0, Time: 1840 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -3,21 +3,22 @@
 using namespace std;
 
 typedef vector<int> vi;
-int v[10000];
+vi v;
 
 int main() {
     int N;
     cin >> N;
 
-    for(int i=0; i < N; ++i) cin >> v[i];
-    sort(v, v + N);
+    v.resize(N);
+    for (int &i: v) cin >> i;
+    sort(v.begin(), v.end());
     long long result{0};
 
     for(int i=0; i < N - 2; ++i){
         for(int j = i + 1; j < N - 1; ++j){
             int cur{v[i] + v[j]};
-            auto lo = lower_bound(v + j + 1, v + N, -cur) - v;
-            auto hi = upper_bound(v + j + 1, v + N, -cur) - v;
+            auto lo = lower_bound(v.begin() + j + 1, v.end(), -cur);
+            auto hi = upper_bound(v.begin() + j + 1, v.end(), -cur);
 //            cout << cur << " " << *lo << " " << *hi << "\n";
             result += (int)(hi - lo);
         }
