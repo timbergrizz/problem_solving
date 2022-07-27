0071dd0f65bae530b399aef13b128775b050dc82
timbergrizzzz <50934118+timbergrizz@users.noreply.github.com>
2022-07-27 16:12:54 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 180 ms, Memory: 4368 KB -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 
 #define FOR(i, init, N) for(ll i=init; i< N; ++i)
-#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MOD 1000000007
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
 typedef long long ll;
 typedef vector<ll> vl;
-const ll mod = 1e9 + 7;
+const ll mod = MOD;
 
 ll power(ll n, ll p) {
     if (p <= 1) return (p ? n : 1);
-    if (p % 2) return (n * power(n, p - 1) % mod) % mod;
+    if (p % 2) return (n * power(n, p - 1) % MOD) % MOD;
 
-    ll ret = (power(n, p / 2)) % mod;
-    return (ret * ret) % mod;
+    ll ret = (power(n, p / 2)) % MOD;
+    return (ret * ret) % MOD;
 }
 
 
@@ -28,9 +29,8 @@ int main() {
 
     ll ans{0};
     FOR(i, 0, N) {
-        ans += v[i] * (power(2, i) + mod - power(2, (ll) N - i - 1)) % mod;
-        ans %= mod;
+        ans = (ans + (v[i] * (power(2, i) + mod - power(2, N - i - 1)))) % mod;
+//        cout << ans << endl;
     }
-
-    cout << ans << "\n";
+    cout << ans << endl;
 }
